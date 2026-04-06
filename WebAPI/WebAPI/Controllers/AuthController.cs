2628179c1bf3b9100a9ef7abe7cb584056178d37
Andrey <andrey.trotsenko.ru@gmail.com>
2026-04-06 17:42:37 +0700

    Add AuthController

---
+++ WebAPI/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TodoDb _db;
+
+        public AuthController(TodoDb db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(LoginDto dto)
+        {
+            var user = await _db.User
+                .FirstOrDefaultAsync(u =>
+                    u.Login == dto.Login &&
+                    u.Password == dto.Password);
+
+            if (user == null)
+                return Unauthorized("Неверный логин или пароль");
+
+            return Ok(user);
+        }
+
+    [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(RegisterDto dto)
+        {
+            var exists = await _db.User.AnyAsync(u => u.Login == dto.Login);
+
+            if (exists)
+                return BadRequest("Пользователь уже существует");
+
+            var user = new User
+            {
+                Login = dto.Login,
+                Password = dto.Password,
+                LastName = dto.LastName,
+                FirstName = dto.FirstName,
+                MiddleName = dto.MiddleName,
+                Email = dto.Email
+            };
+
+            _db.User.Add(user);
+            await _db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/DTO/RegisterDto.cs
@@ -0,0 +1,9 @@
+﻿public class RegisterDto
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string MiddleName { get; set; }
+    public string Email { get; set; }
+}
